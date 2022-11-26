f4a20639ba3ea98143cecffb3da6cc996b736f28
Sangjin Han <49024115+eu2525@users.noreply.github.com>
2022-11-26 23:43:18 +0900

    Update tcp_echo_server_multithread.cpp

--- tcp_echo_server_multithread.cpp
+++ tcp_echo_server_multithread.cpp
@@ -8,25 +8,6 @@ enum ePort { SERVER_PORT = 65456 };
 
 #define _winsock_deprecated_no_warnings
 
-int sendall(int s, char* buf, int* len)
-{
-	int total = 0; // how many bytes we've sent
-	int bytesleft = *len; // how many we have left to send
-	int n = 0;
-	while (total < *len) {
-		n = send(s, buf + total, bytesleft, 0);
-		if (n == -1) {
-			/* print/log error details */
-			break;
-		}
-		total += n;
-		bytesleft -= n;
-	}
-	*len = total; // return number actually sent here
-	return n == -1 ? -1 : 0; // return -1 on failure, 0 on success
-}
-
-
 int main()
 {
 	cout << "> echo-server-multithread is activated" << endl;
@@ -35,7 +16,7 @@ int main()
 	WSAStartup(MAKEWORD(2, 2), &wsa);
 
 	//socket()
-	SOCKET listeningSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); // 3¹ø Â° ÀÎÀÚ 0µµ OK
+	SOCKET listeningSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); // 3ë²ˆ ì§¸ ì¸ìž 0ë„ OK
 	if (listeningSock == INVALID_SOCKET)
 	{
 		cerr << "Can't create a socket! Quitting" << endl;
@@ -44,7 +25,7 @@ int main()
 	}
 
 	//bind()
-	SOCKADDR_IN hint = {}; //ÃÊ±âÈ­ ±ÇÀå»çÇ×
+	SOCKADDR_IN hint = {}; //ì´ˆê¸°í™” ê¶Œìž¥ì‚¬í•­
 	hint.sin_family = AF_INET;
 	hint.sin_port = htons(SERVER_PORT);
 	hint.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
@@ -88,13 +69,13 @@ int main()
 		if (WSAEnumNetworkEvents(sockets[idx], events[idx], &networkEvents) == SOCKET_ERROR)
 			break;
 
-		//acceptÇÑ socket¿¡ event ¿¬°á
+		//acceptí•œ socketì— event ì—°ê²°
 		if (networkEvents.lNetworkEvents & FD_ACCEPT) {
 			sockaddr_in address = {};
 			int size = sizeof(address);
 			SOCKET client = accept(sockets[idx], (sockaddr*)&address, &size);
 			
-			cout << "> client connected by IP address " << inet_ntoa(address.sin_addr) << " with Port nubmer" << address.sin_port << endl;
+			cout << "> client connected by IP address " << inet_ntoa(address.sin_addr) << " with Port number" << address.sin_port << endl;
 
 
 			WSAEVENT cEvt = WSACreateEvent();
@@ -104,7 +85,7 @@ int main()
 			events[counter] = cEvt;
 			counter++;
 		}
-		//recv¿Í send------------------------
+		//recvì™€ send------------------------
 		if (networkEvents.lNetworkEvents & FD_READ) {
 			char buffer[128];
 			int readBytes = recv(sockets[idx], buffer, 128, 0);
@@ -112,7 +93,7 @@ int main()
 			{
 				cout << "> echoed: " << buffer << " by Thread-" << sockets[idx] << endl;
 
-				int a = send(sockets[idx], buffer, strlen(buffer), 0);
+				send(sockets[idx], buffer, strlen(buffer), 0);
 
 				if (strcmp(buffer, "quit") == 0)
 					break;
